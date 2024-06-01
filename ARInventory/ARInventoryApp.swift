81ce343f553b74c4209468248896af5569d8fe0f
aydinnkaya <Aydinnkaya1@gmail.com>
2024-06-01 17:01:48 +0300

    Initial Commit

---
+++ ARInventory/ARInventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARInventoryApp.swift
+//  ARInventory
+//
+//  Created by Aydın KAYA on 1.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ARInventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARInventory/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  ARInventory
+//
+//  Created by Aydın KAYA on 1.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Hello, world!")
+
+            Text("Hello, world!")
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
