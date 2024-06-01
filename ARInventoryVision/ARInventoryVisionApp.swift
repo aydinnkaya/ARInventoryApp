9f509c3593bf308abd8aec54e8652530a71727b5
aydinnkaya <Aydinnkaya1@gmail.com>
2024-06-01 17:33:08 +0300

    ARInventoryVision
    AR Inventory Vision to create

---
+++ ARInventoryVision/ARInventoryVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARInventoryVisionApp.swift
+//  ARInventoryVision
+//
+//  Created by Aydın KAYA on 1.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ARInventoryVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARInventoryVision/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ARInventoryVision
+//
+//  Created by Aydın KAYA on 1.06.2024.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
